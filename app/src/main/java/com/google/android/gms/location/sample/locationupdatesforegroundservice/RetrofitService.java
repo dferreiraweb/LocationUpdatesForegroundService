17f3b4113ad8630c28dfa617d9c4dcd3eca9fc7b
dferreiraweb <dferreira@webeleven.com.br>
2019-10-25 17:52:24 -0300

    first

---
+++ app/src/main/java/com/google/android/gms/location/sample/locationupdatesforegroundservice/RetrofitService.java
@@ -0,0 +1,18 @@
+package com.google.android.gms.location.sample.locationupdatesforegroundservice;
+
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface RetrofitService {
+
+    @FormUrlEncoded
+    @POST("https://quasar-test-two.free.beeceptor.com")
+    Call<JSONObject> carregarDadosLocation(@Field("lat") String lat, @Field("log") String log, @Field("time") String time);
+
+}
